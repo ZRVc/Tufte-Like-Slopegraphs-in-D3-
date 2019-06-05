eb59df2e25375f37af36914561d6f44027030297
ZRVc <49087220+ZRVc@users.noreply.github.com>
2019-06-05 08:13:41 -0700

    Update ROI3.R

--- ROI3.R
+++ ROI3.R
@@ -36,7 +36,7 @@ y2 <- (y1 - drop*(x2.1-x1.1))
 
 y <- c(y1,y2)
 z <- y
-
+space1 <- space
 
 
 ###################################################
@@ -78,9 +78,11 @@ fn3 <- function(y) {
 gr3 <- function(y) {
   return(-1*gr2(y))
 }
-ineq0(y)
+
+
+ineqs <- list(
 ## First set of constraints: ensure that the slopes are drawn on the same scale.
-ineq0 <- function(y,x1=x1.1,x2=x2.1,tol1=tol) {
+ineq1 = function(y,x1=x1.1,x2=x2.1,tol1=tol) {
   
   index1 <- 0
   for(i in 1:length(x1)){
@@ -124,11 +126,11 @@ ineq0 <- function(y,x1=x1.1,x2=x2.1,tol1=tol) {
   v2mod <- v2[2:length(v2)]
   
   return(unname(cbind(umod,v1mod,v2mod)))
-}
+},
 
 ## Second set of constraints: ensure that the numbers are displayed in order with the 
 ## correct line spacing.
-ineq1 <- function(y,space=space1) {
+ineq2 = function(y,space=space1) {
   
   y1 <- y[1:(length(y)/2)]
   y2 <- y[(length(y)/2+1):length(y)]
@@ -164,14 +166,14 @@ ineq1 <- function(y,space=space1) {
   v2mod <- v2[2:length(v2)]
   
   return(unname(cbind(umod,v1mod,v2mod)))
-}
+},
 
 ## Third set of constraints:  Except for the endpoints, every number is between two
 ## other numbers.  The in-between number's positioning should be closer to whichever 
 ## of the two numbers is closer.  I.e., if 37.5 is between 39.0 and 35.2, it should
 ## be positioned closer to 39.0, because 39.0 - 37.5 < 37.5 - 35.2.  This constraint 
 ## is for the first column.
-ineq2 <- function(y,x1=x1.1,x2=x2.1) {
+ineq3 = function(y,x1=x1.1,x2=x2.1) {
   
   y1 <- y[(length(y)/2+1):length(y)]
   
@@ -217,11 +219,11 @@ ineq2 <- function(y,x1=x1.1,x2=x2.1) {
   v2mod <- v2[2:length(v2)]
   
   return(unname(cbind(umod,v1mod,v2mod)))
-}
+},
 
 ## Fourth set of constraints:  This is the same as the third constraint, but it's for
 ## the second column.
-ineq3 <- function(y,x1=x1.1,x2=x2.1) {
+ineq4 = function(y,x1=x1.1,x2=x2.1) {
   
   y2 <- y[(length(y)/2+1):length(y)]
   
@@ -267,9 +269,9 @@ ineq3 <- function(y,x1=x1.1,x2=x2.1) {
   v2mod <- v2[2:length(v2)]
   
   return(unname(cbind(umod,v1mod,v2mod)))
-}
+},
 ## Constraints for first wave of optimization:
-ineq4 <- function(y) {
+ineq6 = function(y) {
   
   y1 <- y[1:(length(y)/2)]
   y2 <- y[(length(y)/2+1):length(y)]
@@ -299,15 +301,18 @@ ineq4 <- function(y) {
   
   return(unname(cbind(umod,v1mod,v2mod)))
 }
+)
 
-inequalitymaker1 <- function(y){
+inequalitymaker1 <- function(y,const = constr){
   
   iq <- rep(0,(length(y)+2))
   
-  iq <- rbind(iq,ineq0(y))
-  iq <- rbind(iq,ineq4(y))
-  iq <- rbind(iq,ineq2(y))
-  iq <- rbind(iq,ineq3(y))
+  for(i in 1:6) {
+    if(i %in% const) {
+      iq <- rbind(iq,ineqs[[i]](y))
+    }
+  }
+
   iq <- iq[2:length(iq[,1]),]
   
   dir1 <- iq[,(length(y)+1)]
@@ -317,9 +322,8 @@ inequalitymaker1 <- function(y){
   rhs1 <- iq[,(length(y)+2)]
   
   iq <- iq[,1:length(y)]
-  
+
   return(L_constraint(L=iq, dir=dir1, rhs=rhs1))
-  
 }
 
 ##################################  SOLVING1  #########################################
@@ -329,13 +333,6 @@ lc1 <- inequalitymaker1(y)
 
 prob1 <- OP(fo1,lc1)
 sol1 <- ROI_solve(prob1,solver="alabama",start=y)
-
-points0 <- solution(sol1)
-points0[4]-points0[3]
-
-
-
-
 
 
 
@@ -430,8 +427,8 @@ index1 <- which(x1 %in% x1[duplicated(x1)])
 index2 <- which(x2 %in% x2[duplicated(x2)])
 
 #if(length(index1) > 0)
-  
-  
+
+
 y1.1 <- y1[index1]
 y2.1 <- y2[index1]
 y1.1tab <- table(y1.1)
@@ -474,34 +471,34 @@ ineq3 <- function(y,x1=x1.1,x2=x2.1) {
     
     for(j in lowernabrs) {
       for(h in highernabrs) {
-    if(x2[i] - x2[j] > x2[h] - x2[i]) {
-      w <- rep(0,(length(y)/2))
-      w[j] <- 1
-      w[h] <- 1
-      w[i] <- -2
-      w1 <- c(rep(0,(length(y)/2)),w)
-      u <- rbind(u,w1)
-      v1 <- append(v1,2,after=length(v1))
-      v2 <- append(v2,0,after=length(v2)) 
-    }
-    if(x2[i] - x2[j] < x2[h] - x2[i]) {
-      w <- rep(0,(length(y)/2))
-      w[j] <- -1
-      w[h] <- -1
-      w[i] <- 2
-      w1 <- c(rep(0,(length(y)/2)),w)
-      u <- rbind(u,w1)
-      v1 <- append(v1,2,after=length(v1))
-      v2 <- append(v2,0,after=length(v2)) 
-    }
+        if(x2[i] - x2[j] > x2[h] - x2[i]) {
+          w <- rep(0,(length(y)/2))
+          w[j] <- 1
+          w[h] <- 1
+          w[i] <- -2
+          w1 <- c(rep(0,(length(y)/2)),w)
+          u <- rbind(u,w1)
+          v1 <- append(v1,2,after=length(v1))
+          v2 <- append(v2,0,after=length(v2)) 
+        }
+        if(x2[i] - x2[j] < x2[h] - x2[i]) {
+          w <- rep(0,(length(y)/2))
+          w[j] <- -1
+          w[h] <- -1
+          w[i] <- 2
+          w1 <- c(rep(0,(length(y)/2)),w)
+          u <- rbind(u,w1)
+          v1 <- append(v1,2,after=length(v1))
+          v2 <- append(v2,0,after=length(v2)) 
+        }
       }
     }
-    }
-    umod <- u[2:dim(u)[1],]
-    v1mod <- v1[2:length(v1)]
-    v2mod <- v2[2:length(v2)]
-    
-    return(unname(cbind(umod,v1mod,v2mod)))
   }
-    
-ineq3(y)
+  umod <- u[2:dim(u)[1],]
+  v1mod <- v1[2:length(v1)]
+  v2mod <- v2[2:length(v2)]
+  
+  return(unname(cbind(umod,v1mod,v2mod)))
+}
+
+ineq4(y)
